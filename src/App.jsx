d904e6a109c8148d25ab211a1fffec01bae8634e
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-08 14:59:01 +0200

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Product from './pages/Product';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={ <Home /> } />
+        <Route path='/products' element={ <ProductList /> } />
+        <Route path='/products/:productId' element={ <Product /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/register' element={ <Register /> } />
+        <Route path='/cart' element={ <Cart /> } />
       </Routes>
     </Router>
   );
